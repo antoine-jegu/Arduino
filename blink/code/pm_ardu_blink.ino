bd8abbcbd1392f631ab295a300e186e0fe06e5ec
pmottier53 <philippe.mottier@gmail.com>
2017-01-10 15:34:20 +0100

    Add files via upload

---
+++ blink/code/pm_ardu_blink.ino
@@ -0,0 +1,19 @@
+void setup()
+{
+Serial.begin(9600);
+pinMode( 13 , OUTPUT);
+}
+
+void loop()
+{
+Serial.print( "Allumage de la LED" );
+Serial.println("");
+digitalWrite( 13 , HIGH );
+delay( 1000 );
+Serial.print( "Extinction de la LED" );
+Serial.println("");
+digitalWrite( 13 , LOW );
+delay( 1000 );
+}
+
+
